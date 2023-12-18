2653cf651c6370164dfd4580c986422c3e755066
ishu28ram <109555964+ishu28ram@users.noreply.github.com>
2023-12-18 23:25:36 +0530

    Update script.js

--- Notes_app/script.js
+++ Notes_app/script.js
@@ -9,8 +9,7 @@ const updateBtnEle = document.querySelector(".updateBtn");
 const descriptionNotesEle = document.querySelector("#descriptionNotes");
 const titleNoteEle = document.querySelector("#titleNotes");
 
-let notesData;
-let notes = [];
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
 let isUpdated = false,
   updateId;
 let months = [
@@ -62,7 +61,6 @@ updateBtnEle.addEventListener("click", (e) => {
       notes[updateId] = note;
     }
     localStorage.setItem("notes", JSON.stringify(notes));
-    notesData = JSON.parse(localStorage.getItem("notes")) || [];
     descriptionNotesEle.value = "";
     titleNoteEle.value = "";
     showModalContainerEle.style.display = "none";
@@ -72,7 +70,7 @@ updateBtnEle.addEventListener("click", (e) => {
 
 function showNewCardNotes() {
   document.querySelectorAll(".notes-card").forEach((note) => note.remove());
-  notesData.forEach((note, index) => {
+  notes.forEach((note, index) => {
     let noteCard = `
         <div class='notes-card'>
               <h2>${note.title}</h2>
@@ -96,8 +94,7 @@ function showNewCardNotes() {
 }
 
 function deleteNotesHandler(ele) {
-  console.log(ele);
-  notesData.splice(ele, 1);
+  notes.splice(ele, 1);
   localStorage.setItem("notes", JSON.stringify(notes));
   showNewCardNotes();
 }
@@ -110,83 +107,3 @@ function editNotesHandler(index, title, desc) {
   updateBtnEle.textContent = "Update ";
   document.querySelector(".modal-header h2").textContent = "Update A Note";
 }
-// function deleteHandler() {
-//   for (let i = 0; i < deleteNotesBtnEle.length; i++) {
-//     deleteNotesBtnEle[i].addEventListener("click", () => {
-//       deleteNotesBtnEle[i].parentElement.remove();
-//     });
-//   }
-// }
-// addNotesBtnEle.addEventListener("click", () => {
-//   showModalContainerEle.style.display = "block";
-// });
-// removeModalBtnEle.addEventListener("click", () => {
-//   showModalContainerEle.style.display = "none";
-// });
-// updateBtnEle.addEventListener("click", (e) => {
-//   e.preventDefault;
-//   let title = titleNoteEle.value;
-//   let desc = descriptionNotesEle.value;
-//   showModalContainerEle.style.display = "none";
-//   if (!desc || !title) return alert("Please fill out all fields!");
-//   let notes = [];
-//   let note = {
-//     title: title,
-//     desc: desc,
-//   };
-//   notes.push(note);
-
-//   localStorage.setItem("notes", JSON.stringify(notes));
-//   addNewNotesCardHandler();
-//   titleNoteEle.value = "";
-//   descriptionNotesEle.value = "";
-// });
-// function addNewNotesCardHandler() {
-//   let newNotesCard = document.createElement("div");
-//   newNotesCard.classList.add("notes-card");
-
-//   notesData.forEach((note, index) => {
-//     newNotesCard.innerHTML = `
-//               <h2>${note.title}</h2>
-//               <textarea readonly>${note.desc}</textarea>
-//               <div class="notes-controls">
-
-//                 <span class="edit-control" onclick='editHandler(${index},'${notes.title}','${desc.title}')
-//                   ><i class="fa-solid fa-pencil"></i
-//                 ></span>
-//                 <span class="delete-control" onclick='deleteHandler(${index})'
-//                   ><i class="fa-solid fa-trash"></i
-//                 ></span>
-//               </div>
-//       `;
-//   });
-
-//   // notesContainerEle.appendChild(newNotesCard);
-//   // let deleteBtns = notesContainerEle.querySelectorAll(
-//   //   ".notes-card .notes-controls .delete-control"
-//   // );
-//   // let editBtns = notesContainerEle.querySelectorAll(
-//   //   ".notes-card .notes-controls .edit-control"
-//   // );
-//   // deleteBtns.forEach((delBtn) => {
-//   //   delBtn.addEventListener("click", () => {
-//   //     delBtn.parentElement.parentElement.remove();
-//   //   });
-//   // });
-//   // editBtns.forEach((editBtn) => {
-//   //   editBtn.addEventListener("click", () => {
-//   //     let main = editBtn.parentElement.parentElement;
-//   //     let desc = main.querySelector("textarea").value;
-//   //     let title = main.querySelector("h2").textContent;
-//   //     console.log(desc, title);
-//   //     editNotesHandler(desc, title);
-//   //   });
-//   // });
-// }
-
-// function editNotesHandler(desc, title) {
-//   showModalContainerEle.style.display = "block";
-//   descriptionNotesEle.value = desc;
-//   titleNoteEle.value = title;
-//   addNewNotesCardHandler(desc, title);
-// }
